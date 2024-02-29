e3dbdc695d244aff8db44656e8ce66b853a26970
radian628 <70352880+radian628@users.noreply.github.com>
2024-02-28 16:14:16 -0800

    made typedefs public

--- app/src/main/java/radian628/bathrooms/TypeDefs.kt
+++ app/src/main/java/radian628/bathrooms/TypeDefs.kt
@@ -1,23 +1,33 @@
 package radian628.bathrooms
 
-enum class BathroomGender {
+public enum class BathroomGender {
     MALE, FEMALE, INCLUSIVE
 }
 
-data class Bathroom(
+public data class Bathroom(
+    // should be same as it appears IRL
     val room_number: String,
+
+    // not sure whether to use building name or an ID linked to it
+    // especially since the Locations API doesn't use name
     val building_id: String,
+
     val gender: BathroomGender,
     val wheelchair_accessible: Boolean,
+
+    // ranges from 1 to 5
     val rating: Double
 )
-data class Floor(
+
+public data class Floor(
     val name: String,
     val bathrooms: List<Bathroom>
 )
 
-data class Building(
+public data class Building(
     val floors: List<Floor>,
+
+    // coordinates
     val lat: Double,
     val lon: Double
 )
